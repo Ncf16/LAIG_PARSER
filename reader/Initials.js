4910cad2a0baef1fbc2b3b2509bf89f374e0a436
Ncf16 <joaofilipe26@gmail.com>
2015-10-19 23:22:28 +0100

    comments

--- reader/Initials.js
+++ reader/Initials.js
@@ -1,9 +1,9 @@
-//check types
-
+//Creates a new object of the class Initials
 function initials() {
+    //Default values
     this.frustum = new Frustum();
     this.axisLength = 1;
- 
+
 };
 initials.prototype.getNear = function() {
     return this.frustum.near;
@@ -12,21 +12,21 @@ initials.prototype.getFar = function() {
     return this.frustum.far;
 };
 initials.prototype.setNear = function(newNear) {
-    this.frustum.near = newNear;
+    this.frustum.setNear(newNear);
 };
 initials.prototype.setFar = function(newFar) {
-    this.frustum.far = newFar;
+    this.frustum.setFar(newFar);
 };
 initials.prototype.setAxisLength = function(newLength) {
+    //validades the length given
     if (validadeNumber(newLength, function(x) {
-            if (x > 0)
-                return true;
-            else
-                return false;
-        }))
+        if (x > 0)
+            return true;
+        else
+            return false;
+    }))
         this.axisLength = newLength;
 };
 initials.prototype.getAxisLength = function() {
     return this.axisLength;
 };
- 
