97d195ff9b0e7a84adeca34fbb63a135766218e7
Ncf16 <joaofilipe26@gmail.com>
2015-10-19 19:57:07 +0100

    some changes

--- reader/Lights.js
+++ reader/Lights.js
@@ -1,59 +1,60 @@
-var CONVERTER=1;
+var CONVERTER = 1;
+
 function Lights() {
-	this.enabled = false;
-	this.ID = "";
-	this.pos;
-	this.amb;
-	this.diff;
-	this.spec;
+    this.enabled = false;
+    this.ID = "";
+    this.pos;
+    this.amb;
+    this.diff;
+    this.spec;
 };
 
 Lights.prototype.isEnabled = function() {
-	return this.enabled;
+    return this.enabled;
 };
 Lights.prototype.getID = function() {
-	return this.ID;
+    return this.ID;
 };
 Lights.prototype.getPosition = function() {
-	return this.pos;
+    return this.pos;
 };
 Lights.prototype.getAmbient = function() {
-	return this.amb;
+    return this.amb;
 };
 Lights.prototype.getDiffuse = function() {
-	return this.diff;
+    return this.diff;
 };
 Lights.prototype.getSpecular = function() {
-	return this.spec;
+    return this.spec;
 };
 Lights.prototype.setEnabled = function(newEnabled) {
-	var temp = Number(newEnabled);
-	if (validadeNumber(temp, function(x) {
-			if ((x != 0 && x != 1) || isNaN(x))
-				return false;
-			else
-				return true;
-		})) {
-		this.enabled = temp;
-	} else {
-		console.warn("Inpute in the Enable section of a Light was invalide, the default value will be used (false)");
-		this.enabled = 0;
-	}
+    var temp = Number(newEnabled);
+    if (validadeNumber(temp, function(x) {
+        if ((x != 0 && x != 1) || isNaN(x))
+            return false;
+        else
+            return true;
+    })) {
+        this.enabled = temp;
+    } else {
+        console.warn("Inpute in the Enable section of a Light was invalide, the default value will be used (false)");
+        this.enabled = 0;
+    }
 };
 
 
 Lights.prototype.setID = function(newID) {
-	this.ID = newID;
+    this.ID = newID;
 }
 Lights.prototype.setPosition = function(newPos) {
-	this.pos = stringArrayToNumber(newPos, "Position", "inf", "inf", 1);
+    this.pos = stringArrayToNumber(newPos, "Position", "inf", "inf", 1);
 };
 Lights.prototype.setAmbient = function(newAmb) {
-	this.amb = stringArrayToNumber(newAmb, "RGB", 0, 1, CONVERTER);
+    this.amb = stringArrayToNumber(newAmb, "RGB", 0, 1, CONVERTER, 255);
 };
 Lights.prototype.setDiffuse = function(newDiff) {
-	this.diff = stringArrayToNumber(newDiff, "RGB", 0, 1, CONVERTER);
+    this.diff = stringArrayToNumber(newDiff, "RGB", 0, 1, CONVERTER, 255);
 };
 Lights.prototype.setSpecular = function(newSpec) {
-	this.spec = stringArrayToNumber(newSpec, "RGB", 0, 1, CONVERTER);
+    this.spec = stringArrayToNumber(newSpec, "RGB", 0, 1, CONVERTER, 255);
 };
--- reader/Materials.js
+++ reader/Materials.js
@@ -1,72 +1,72 @@
 function Materials() {
-	this.shininess = 0;
-	this.ID = "";
-	this.emission = [];
-	this.amb = [];
-	this.diff = [];
-	this.spec = [];
+    this.shininess = 0;
+    this.ID = "";
+    this.emission = [];
+    this.amb = [];
+    this.diff = [];
+    this.spec = [];
 };
 
 Materials.prototype.getShininess = function() {
-	return this.shininess;
+    return this.shininess;
 };
 Materials.prototype.getID = function() {
-	return this.ID;
+    return this.ID;
 };
 Materials.prototype.getEmission = function() {
-	return this.emission;
+    return this.emission;
 };
 Materials.prototype.getAmbient = function() {
-	return this.amb;
+    return this.amb;
 };
 Materials.prototype.getDiffuse = function() {
-	return this.diff;
+    return this.diff;
 };
 Materials.prototype.getSpecular = function() {
-	return this.spec;
+    return this.spec;
 };
 Materials.prototype.setShininess = function(newShininess) {
 
-	var temp = Number(newShininess);
-	if (validadeNumber(temp, function(x) {
-		if (x < 0 || x == NaN)
-			return false;
-		else
-			return true;
-	})) {
-		this.shininess = temp;
-	} else {
-		console.warn("Inpute in the Shininess section of a Material was invalide, the default value will be used (50)");
-		this.shininess = 50;
-	}
+    var temp = Number(newShininess);
+    if (validadeNumber(temp, function(x) {
+        if (x <= 0 || x == NaN)
+            return false;
+        else
+            return true;
+    })) {
+        this.shininess = temp;
+    } else {
+        console.warn("Inpute in the Shininess section of a Material was invalide, the default value will be used (50)");
+        this.shininess = 50;
+    }
 };
 Materials.prototype.setID = function(newID) {
-	this.ID = newID;
+    this.ID = newID;
 };
 Materials.prototype.setEmission = function(newEmission) {
-	this.emission = stringArrayToNumber(newEmission, "RGB", 0, 255, 255);
+    this.emission = stringArrayToNumber(newEmission, "RGB", 0, CONVERTER, 255);
 };
 Materials.prototype.setAmbient = function(newAmbient) {
-	this.amb = stringArrayToNumber(newAmbient, "RGB", 0, 255, 255);
+    this.amb = stringArrayToNumber(newAmbient, "RGB", 0, CONVERTER, 255);
 };
 Materials.prototype.setDiffuse = function(newDiffuse) {
-	this.diff = stringArrayToNumber(newDiffuse, "RGB", 0, 255, 255);
+    this.diff = stringArrayToNumber(newDiffuse, "RGB", 0, CONVERTER, 255);
 };
 Materials.prototype.setSpecular = function(newSpecular) {
-	this.spec = stringArrayToNumber(newSpecular, "RGB", 0, 255, 255);
+    this.spec = stringArrayToNumber(newSpecular, "RGB", 0, CONVERTER, 255);
 };
 
-Materials.prototype.setAppearence = function(scene){
+Materials.prototype.setAppearence = function(scene) {
 
-	this.appearance = new CGFappearance(scene);
-	this.appearance.setAmbient(this.amb[0], this.amb[1], this.amb[2], this.amb[3]);
-	this.appearance.setDiffuse(this.diff[0], this.diff[1], this.diff[2], this.diff[3]);
-	this.appearance.setSpecular(this.spec[0], this.spec[1], this.spec[2], this.spec[3]);
-	this.appearance.setEmission(this.emission[0], this.emission[1], this.emission[2], this.emission[3]);
-	this.appearance.setShininess(this.getShininess());
+    this.appearance = new CGFappearance(scene);
+    this.appearance.setAmbient(this.amb[0], this.amb[1], this.amb[2], this.amb[3]);
+    this.appearance.setDiffuse(this.diff[0], this.diff[1], this.diff[2], this.diff[3]);
+    this.appearance.setSpecular(this.spec[0], this.spec[1], this.spec[2], this.spec[3]);
+    this.appearance.setEmission(this.emission[0], this.emission[1], this.emission[2], this.emission[3]);
+    this.appearance.setShininess(this.getShininess());
 }
 
-Materials.prototype.setTexture = function(texture){
+Materials.prototype.setTexture = function(texture) {
 
-	this.appearance.setTexture(texture);
+    this.appearance.setTexture(texture);
 }
