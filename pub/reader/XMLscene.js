cded632b98a971822bcf59b94afe4b4ac0d631d0
ruiProg <ruifilipeosorio17@hotmail.com>
2015-12-24 07:28:25 +0000

    no need for log picking anymore

--- pub/reader/XMLscene.js
+++ pub/reader/XMLscene.js
@@ -443,25 +443,6 @@ XMLscene.prototype.replayMove = function(move) {
     play(this, move);
 }
 
-<<<<<<< HEAD
-=======
-XMLscene.prototype.logPicking = function() {
-
-    if (this.pickMode == false) {
-        if (this.pickResults != null && this.pickResults.length > 0) {
-            for (var i = 0; i < this.pickResults.length; i++) {
-                var obj = this.pickResults[i][0];
-                if (obj) {
-                    var customId = this.pickResults[i][1];
-                    console.log("Picked object: " + obj + ", with pick id " + customId);
-                }
-            }
-            this.pickResults.splice(0, this.pickResults.length);
-        }
-    }
-}
-
->>>>>>> fa0fbea2fda419827658e1fe2b17dc1093a5ce90
 XMLscene.prototype.display = function() {
 
     this.graph.movTrack.listen();
